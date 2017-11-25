3e657ef3ef3aa66ee85c16a7f3b9d640c545e5ec
Samuel Susla <samuel.susla@gmail.com>
2017-11-25 13:17:10 +0000

    Add more tests

--- Tests/GPXTests/TrackTests.swift
+++ Tests/GPXTests/TrackTests.swift
@@ -2,7 +2,13 @@ import XCTest
 @testable import GPX
 
 final class TrackTests: XCTestCase {
-    func testGenerateNode() {
+    var node: Track!
+    override func setUp() {
+        super.setUp()
+        node = Track()
+        node.segments.append(TrackSegment.make())
+    }
+    func testGenerateNodeWithNameAndDescription() {
         var node = Track()
         node.name = "Test name"
         node.description = "Test description"
@@ -20,7 +26,39 @@ final class TrackTests: XCTestCase {
         XCTAssertEqual(node.generateNode(), expectedResult)
     }
 
+    func testGenerateNodeWithName() {
+        var node = Track()
+        node.name = "Test name"
+        node.segments.append(TrackSegment.make())
+
+        let expectedResult = """
+        <trk>
+            <name>Test name</name>
+            <trkseg>
+                <trkpt lat="10.0" lon="10.0"></trkpt>
+            </trkseg>
+        </trk>
+        """
+        XCTAssertEqual(node.generateNode(), expectedResult)
+    }
+
+    func testGenerateNode() {
+        var node = Track()
+        node.segments.append(TrackSegment.make())
+
+        let expectedResult = """
+        <trk>
+            <trkseg>
+                <trkpt lat="10.0" lon="10.0"></trkpt>
+            </trkseg>
+        </trk>
+        """
+        XCTAssertEqual(node.generateNode(), expectedResult)
+    }
+
     static var allTests = [
-        ("testGenerateNode", testGenerateNode),
+        ("testGenerateNodeWithNameAndDescription", testGenerateNodeWithNameAndDescription),
+        ("testGenerateNodeWithName", testGenerateNodeWithName),
+        ("testGenerateNode", testGenerateNodeWithName)
         ]
 }
