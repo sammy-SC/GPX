819dc1187d61b4a4d037d56ed05128e2559e69ea
Samuel Susla <samuel.susla@gmail.com>
2017-11-25 13:13:35 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,26 +3,9 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "GPX",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "GPX",
-            targets: ["GPX"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "GPX",
-            dependencies: []),
-        .testTarget(
-            name: "GPXTests",
-            dependencies: ["GPX"]),
-    ]
-)
+let package = Package(name: "GPX", products: [.library(name: "GPX", targets: ["GPX"])],
+            targets: [
+                .target(name: "GPX"),
+                .testTarget(name: "GPXTests", dependencies: ["GPX"]),
+            ],
+            swiftLanguageVersions: [4])
