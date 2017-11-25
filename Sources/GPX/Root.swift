67f18435a738adb164de9c2710a2fe6770318b1f
Samuel Susla <samuel.susla@gmail.com>
2017-11-25 17:53:57 +0000

    Change access modifiers

--- Sources/GPX/Root.swift
+++ Sources/GPX/Root.swift
@@ -1,13 +1,13 @@
 public struct Root: GPXNode {
-    let creator: String
+    private let creator: String
     private let version = "1.1"
-    var tracks = [Track]()
+    public var tracks = [Track]()
 
     public init(creator: String) {
         self.creator = creator
     }
 
-    func generateNode() -> String {
+    public func generateNode() -> String {
         var result = """
         <?xml version="1.0" encoding="UTF-8"?>
         <gpx xmlns="http://www.topografix.com/GPX/1/1" version="\(version)" creator="\(creator)">
